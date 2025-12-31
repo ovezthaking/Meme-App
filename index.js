d51c29e6a290ef76db3555dccaf570e752946d5a
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-31 15:33:31 +0100

    Refactor: Update renderEmotionsRadios to display emotions as radio buttons

--- index.js
+++ index.js
@@ -18,7 +18,17 @@ function renderEmotionsRadios(cats){
     const emotions = getEmotionsArray(cats)
     let radioItems = ''
     for (let emotion of emotions) {
-        radioItems += `<p>${emotion}</p>`
+        radioItems += `
+            <div class="radio">
+                <label for="${emotion}">${emotion}</label>
+                <input 
+                type='radio'
+                id='${emotion}'
+                name='emotions'
+                value='${emotion}'
+                >
+            </div>
+        `
     }
     emotionRadios.innerHTML = radioItems
 }
