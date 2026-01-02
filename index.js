262f9b627afeb2194594f4f719e8867326c9a081
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-02 03:31:32 +0100

    Refactor: Add highlightCheckedOption function to manage radio button selection

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ import { catsData } from "./data.js"
 
 const emotionRadios = document.getElementById('emotion-radios')
 
+emotionRadios.addEventListener('change', highlightCheckedOption)
+
+function highlightCheckedOption(e){
+    const radios = document.getElementsByClassName('radio')
+    for (let radio of radios){
+        radio.classList.remove('highlight')
+    }
+
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+
 function getEmotionsArray(cats){
     const emotionsArray = []
 
